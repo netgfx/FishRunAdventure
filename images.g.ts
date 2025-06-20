fcf95997075564d17ca6b944dd39c9c50cdcab28
Michael Dobekidis <mdobekidis@netgfx.com>
2025-06-20 21:21:20 +0300

    Initial files for MakeCode project

---
+++ images.g.ts
@@ -0,0 +1,120 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image1":
+            case "myImage":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`;
+            case "image2":
+            case "myImage0":return img`
+5 5 c c 7 5 c 5 c 5 7 7 c c 5 5 
+5 5 c c 3 9 2 7 2 9 3 3 c c 5 5 
+c c c c 3 9 7 5 7 9 3 3 c c c c 
+7 3 3 3 3 9 2 7 2 9 3 3 3 3 3 7 
+7 3 3 3 3 9 4 4 4 9 3 3 3 3 3 7 
+7 3 3 3 3 9 2 5 2 9 3 3 3 3 3 7 
+5 9 2 9 4 7 7 7 7 7 2 9 2 9 2 5 
+c 9 7 9 2 7 c c c 7 4 9 7 9 4 c 
+5 7 4 7 5 7 c 5 c 7 5 7 c 7 2 5 
+c 9 7 9 2 7 c c c 7 4 9 7 9 4 c 
+5 9 2 9 4 7 7 7 7 7 2 9 2 9 2 5 
+7 3 3 3 3 9 2 5 2 9 3 3 3 3 3 7 
+7 3 3 3 3 9 4 7 4 9 3 3 3 3 3 7 
+c c c c 3 9 7 9 7 9 3 3 c c c c 
+5 5 c c 3 9 4 7 4 9 3 3 c c 5 5 
+5 5 c c 7 5 c 5 c 5 7 7 c c 5 5 
+`;
+            case "image3":
+            case "myImage1":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`;
+            case "image4":
+            case "myImage2":return img`
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+7 4 7 4 7 4 7 4 7 4 7 4 7 4 7 7 
+7 7 4 4 4 4 4 4 4 4 4 4 4 7 4 7 
+7 4 7 b 4 b b 4 4 b 4 b 7 4 7 7 
+7 7 b b c b c b c b c b b 4 4 7 
+7 4 4 c a 7 b c b 7 a c 4 4 7 7 
+7 7 b b c a 7 a 7 a c b b 4 4 7 
+7 4 4 c b 7 a 7 a 7 b c 4 4 7 7 
+7 7 b b c a 7 a 7 a c b b 4 4 7 
+7 4 4 c b 7 a 7 a 7 b c 4 4 7 7 
+7 7 b b c a 7 a 7 a c b b 4 4 7 
+7 4 4 c a 7 b c b 7 a c 4 4 4 7 
+7 7 b b c b c b c b c b b 4 7 7 
+7 4 7 b 4 b 4 b 4 b 4 b 7 7 4 7 
+7 7 4 7 4 7 4 7 4 7 4 7 4 7 7 7 
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+`;
+            case "image5":
+            case "myImage3":return img`
+f 7 7 7 7 7 7 f f 7 7 7 7 7 7 a 
+f 9 a a a a a f f a a a a a b f 
+f f 2 b b b b f f b b b b 4 f f 
+f f f 9 4 4 4 f f 4 4 4 c f f f 
+f f f f 2 c c f f c c 8 f f f f 
+f f f f f 9 8 f f 8 3 f f f f f 
+f f f f f f f f f 5 f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f 5 f f f f f f f f f 
+f f f f f 3 8 f f 8 9 c f f f f 
+f f f f 8 c c f f c c 2 f f f f 
+f f f c 4 4 4 f f 4 4 4 9 f f f 
+f f 4 b b b b f f b b b b 2 f f 
+f b a a a a a f f a a a a a 9 f 
+a 7 7 7 7 7 7 f f 7 7 7 7 7 7 a 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("song", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
